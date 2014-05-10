8ba9b113ca86a24493e2d9ecfe8b7454beed041d
Sir-Mr-Bman <heybrennan@gmail.com>
2014-05-09 22:09:30 -0400

    Added source files.

---
+++ src/me/bman/tickets/Header.java
@@ -0,0 +1,18 @@
+package me.bman.tickets;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/*
+ * 
+ * Bukkit Ticket
+ * By Sir_Mr_Bman
+ * 
+ * DO NOT REDISTRIBUTE!
+ * 
+ */
+
+public class Header extends JavaPlugin {
+	
+	
+
+}
